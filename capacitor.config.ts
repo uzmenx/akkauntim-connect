352314b391369ce75dc4e0895d0dd70ad10597cf
uzmenx <baknistan@gmail.com>
2026-07-21 01:56:41 +0500

    chore: Initialize Capacitor for Android APK builds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uzmenx.akkauntimconnect',
+  appName: 'Akkauntim Connect',
+  webDir: 'dist'
+};
+
+export default config;
